42fd20fb246fabf0d8032e40c7e4877736c879fa
Usike deepika <111295716+usikedeepika@users.noreply.github.com>
2025-05-03 16:55:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+  const prices = document.querySelectorAll(".price");
+    let totalPrice = 0;
+
+
+  
+    prices.forEach(price => {
+      totalPrice += parseFloat(price.textContent) || 0; // Convert text to number
+    });
+  
+
+    console.log("Total Price:", totalPrice);
+   
+      let mytable=document.getElementById("my-table")
+
+      let row=document.createElement('tr');
+
+      row.innerHTML=` <td>Total price</td><td>${totalPrice}</td>`;
+       mytable.appendChild(row);
+
   
 };
 
